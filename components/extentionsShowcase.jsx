20916874c227a21b936f468472753917d188e38d
Luis Ortega <lfortega0625@gmail.com>
2025-05-23 23:49:57 -0400

    now somehow it just works

--- components/extentionsShowcase.jsx
+++ components/extentionsShowcase.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import ExtentionCard from "../components/ExtentionCard";
+import '../components/ExtentionCard.css'
+
 
 import '../components/ExtentionsShowcase.css';
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
-import ExtentionsShowcase from "../components/extentionsShowcase";
+import '../components/ExtentionsShowcase.css';
+import '../components/ExtentionCard.css'
+
 const URL = `./data.json`
 
 
@@ -29,22 +31,111 @@ const FetchData = async(URL, setExtentions) =>{
 
 
 function App() {
-  const [extentions, setExtentions] = useState([])
 
-  useEffect( () => { FetchData( URL, setExtentions ), [] });
+  const [extentions, setExtentions] = useState([])
+  const [currentFilter, setcurrentFilter] = useState("all")
+  const [filteredExtentions, setFilteredExtentions] = useState(extentions)
+
+  useEffect( () => { FetchData( URL, setExtentions ) },[]); // FETCH
+
+  useEffect(()=>{ //FILTER
+
+      switch (currentFilter){
+          case "all":  
+              setFilteredExtentions(extentions) // do nothing
+              break;
+
+          case "active": 
+              setFilteredExtentions( extentions.filter( (item)=> item.isActive===true ) )
+              break;
+
+          case "inactive": 
+              setFilteredExtentions( extentions.filter( (item)=> item.isActive===false ) )
+              break;
+          default: console.log(`there's been an error`)
+      }
+
+  }, [currentFilter, extentions])
+  
+  const handleRadio = (e)=>{ // CHANGE FILTER
+      setcurrentFilter(e.target.value)
+  }
+  
+  const handlecheckbox = ( currentExtention )=>{
+    
+    setExtentions(
+      [...extentions, currentExtention.isActive= !currentExtention.isActive]
+    )
+    console.log(currentExtention)    
+    
+  }
 
   return (
     <>
       <main>
 
         <header>
-          <img src="./assets/images/favicon-32x32.png" alt="./assets/images/logo.svg" />
-          <h2>Extentions</h2>
+          
+          <img src="./assets/images/logo.svg" alt="./assets/images/logo.svg" />
+          
           <button><img src="./assets/images/icon-sun.svg" alt="./assets/images/icon-sun.svg" /></button>
+
+          <h1>Extentions</h1>
+
+          <div className="filterArea">
+              <label htmlFor="all">All</label>
+              <input type="radio" name="selection" id="all" value="all" onChange={ (e)=>handleRadio(e) } defaultChecked/>
+              
+              <label htmlFor="active">Active</label>
+              <input type="radio" name="selection" id="active" value="active" onChange={ (e)=>handleRadio(e) } />
+
+              <label htmlFor="inactive">Inactive</label>
+              <input type="radio" name="selection" id="inactive" value="inactive" onChange={ (e)=>handleRadio(e) } />
+          </div>
+
         </header>
 
-        <ExtentionsShowcase extentions={extentions} setExtentions={setExtentions}/>
-        
+            <ul className="ExtentionsShowcase">
+
+            { filteredExtentions.length>0 && 
+
+              filteredExtentions.map( (extention,index) => {
+
+                return (
+                  <li className="ExtentionCard" key={`extention-${index}`}>
+            
+                      <div>
+                          <picture> <img src={extention.logo} alt={extention.logo}/> </picture>
+                          <div> 
+                              <h4>{extention.name}</h4> 
+                              <p>{extention.description}</p>
+                          </div>
+                      </div>
+                      
+                      <div className="ExtentionCardActions">
+
+                          <button>Remove</button>
+
+                          <input type="checkbox" 
+                                  checked={extention.isActive} 
+                                  id={extention.name}
+                                  name={extention.name}
+                                  onChange={ ()=> handlecheckbox(extention) }
+                          />
+
+                          <label htmlFor={extention.name}></label>
+                          
+                      </div>
+
+                  </li>
+                )
+
+              })
+
+            }
+
+            </ul>
+
       </main>      
     </>
   )
