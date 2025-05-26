9f1718bfcadea7960c87c2749e1eecf1495cc731
Luis Ortega <lfortega0625@gmail.com>
2025-05-26 00:11:17 -0400

    gh-13

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 import ExtentionCard from "./components/ExtentionCard";
 
+
 const URL = `assets/api/data.json` 
 
 const FetchData = async(URL, setExtentions) =>{
@@ -76,7 +77,7 @@ function App() {
             flex justify-between
           `}>
 
-            <img src="./assets/images/logo.svg" alt="./assets/images/logo.svg"/>
+            <img src="assets/images/logo.svg" alt="./assets/images/logo.svg"/>
 
             <button className={`
               ${theme === 'dark' ? 'bg-Neutral700 hover:bg-Neutral600' : 'bg-Neutral200 hover:bg-Neutral100'}
@@ -86,9 +87,9 @@ function App() {
             `} onClick={handleTheme}>
               {theme === 'dark' 
                 ? 
-                <img src="./assets/images/icon-sun.svg" alt="./assets/images/icon-sun.svg" />
+                <img src="assets/images/icon-sun.svg" alt="assets/images/icon-sun.svg" />
                 :
-                <img src="./assets/images/icon-moon.svg" alt="./assets/images//icon-moon.svg" />  
+                <img src="assets/images/icon-moon.svg" alt="assets/images/icon-moon.svg" />  
               }
               
             </button>
