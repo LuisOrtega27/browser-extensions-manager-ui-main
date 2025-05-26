8507c42a0d6a9a02c564f156d7e39c987998f575
Luis Ortega <lfortega0625@gmail.com>
2025-05-25 23:55:57 -0400

    gh-12

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 import ExtentionCard from "./components/ExtentionCard";
 
-const URL = `src/api/data.json` 
+const URL = `assets/api/data.json` 
 
 const FetchData = async(URL, setExtentions) =>{
   
